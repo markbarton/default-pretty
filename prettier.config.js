2691a43cf12c89be75fce1ec73f168f70cddd347
Mark Barton <markbarton@gmail.com>
2024-09-02 14:55:15 +0100

    update for default improt

--- prettier.config.js
+++ prettier.config.js
@@ -1,5 +1,21 @@
 module.exports = {
-    semi: true,
-    singleQuote: true,
-    trailingComma: "es5"
+  semi: true,
+  singleQuote: false,
+  trailingComma: "es5",
+  tabWidth: 2,
+  useTabs: false,
+  endOfLine: "lf",
+  bracketSpacing: true,
+  arrowParens: "always",
+  printWidth: 120,
+  bracketSameLine: true,
+  plugins: [require.resolve("prettier-plugin-sort-imports")],
+  importOrder: [
+    "^react$", // React imports first
+    "<THIRD_PARTY_MODULES>", // Third-party imports
+    "^@/", // Internal imports
+    "^[./]", // Relative imports
+  ],
+  importOrderSeparation: true, // Add a newline between import groups
+  importOrderSortSpecifiers: true, // Sort the specifiers within import statements
 };
